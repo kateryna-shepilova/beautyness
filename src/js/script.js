5eefb2e1264d297d23d260a73480fdcb106c31da
Kateryna Shepilova <kateryna.shepilova@md.p2h.com>
2025-01-14 21:26:34 +0200

    update js, bg image

--- src/js/script.js
+++ src/js/script.js
@@ -11,25 +11,4 @@ document.addEventListener('DOMContentLoaded', function () {
     Fancybox.bind("[data-fancybox]", {
         // Your custom options
     });
-
-    // swiper
-    const swiper = new Swiper('.reviews', {
-        loop: true,
-        slidesPerView: 1,
-        spaceBetween: 24,
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        breakpoints: {
-            768: {
-                slidesPerView: 2,
-                spaceBetween: 16,
-            },
-            1024: {
-                slidesPerView: 3,
-                spaceBetween: 24,
-            },
-        },
-    });
 });
